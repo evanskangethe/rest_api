f65e16432bf1c81327e1623d65d122403c5ccca5
kaka <murira.evans@gmail.com>
2018-11-09 02:53:35 +0300

    make the express server

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 //require modules
-const app = require('express')()
-const http = require('http').Server(app)
-const log = console.log
+const app = require('express')();
+const bodyparser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+const router = require('express').Router();
+const http = require('http').Server(app);
+const log = console.log;
 
+//config database
 const db = require('./config/database')
-const {PORT,URL} = require('./config/props')
+const {PORT, URL} = require('./config/props')
+
+const userRoute = require('./api/users/users.routes')(router);
 
 db()
 
-http.listen(PORT,()=>log(`server connected at http://localhost:${PORT}`))
+//add middleware
+app.use(bodyparser.json())
+app.use(morgan('tiny'))
+app.use(cors)
+app.use(bodyparser.urlencoded({extended: true}))
+app.use('/api',router)
+
+http.listen(PORT, () => log(`server connected at http://localhost:${PORT}`))
