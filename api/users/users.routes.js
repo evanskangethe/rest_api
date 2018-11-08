2ff7526af5e1a3fcdc76caf48df134e149ba73e9
kaka <murira.evans@gmail.com>
2018-11-09 02:40:10 +0300

    make the route

---
+++ api/users/users.routes.js
@@ -0,0 +1,9 @@
+const Users = require('./users.controller');
+
+module.exports = function (router) {
+  router.post('/create',Users.createUser)
+  router.get('/get',Users.getUsers)
+  router.get('/get/:name',Users.getUser)
+  router.put('/update/:id',Users.updateUser)
+  router.delete('/remove/:name',Users.removeUser)
+};
